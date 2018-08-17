e02fbeb72e51554a83b1e27f301db6c57b45c4bb
alanouyang <alanouyang@tencent.com>
2018-08-17 17:06:47 +0800

    钟摆模型demo Signed-off-by: alanouyang <alanouyang@tencent.com>

---
+++ Assets/Shatter Toolkit/Examples/Basic/myforce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myforce : MonoBehaviour {
+
+    bool flag = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!flag)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(0f, 0f, 100f);
+            flag = true;
+        }
+    }
+
+}
---
+++ Assets/explore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explore : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Transform[] items = GetComponentsInChildren<Transform>();
+        foreach (Transform child in items)
+        {
+            BoxCollider collider = child.GetComponent<BoxCollider>();
+            collider.attachedRigidbody.AddExplosionForce(300f, -transform.position, 100, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
