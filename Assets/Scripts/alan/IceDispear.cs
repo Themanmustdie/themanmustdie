69ab37b69c8f69003232c6c884a3b168fba1388e
bindokun <554688613@qq.com>
2018-08-26 22:26:45 +0800

    工程升级为2018.2f

---
+++ Assets/Scripts/alan/IceDispear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class IceDispear : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "floor")
+        {
+            Destroy(gameObject, 2.0f);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
