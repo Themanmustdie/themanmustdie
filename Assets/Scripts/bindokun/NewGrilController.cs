b85a6a22b509eef2722d70017ee04b5588c54744
bindokun <554688613@qq.com>
2018-08-27 23:25:09 +0800

    导入地形和CharacterController

---
+++ Assets/Scripts/bindokun/NewGrilController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGrilController : MonoBehaviour
+{
+    public float speed = 30f;
+    private CharacterController charController;
+    private bool isLeftBtnDown = false;
+    private bool isRightBtnDown = false;
+   
+    public void OnLeftBtnDown()
+    {
+        if (!isRightBtnDown)
+            isLeftBtnDown = true;
+    }
+
+    public void OnLeftBtnUp()
+    {
+        isLeftBtnDown = false;
+    }
+
+    public void OnRightBtnDown()
+    {
+        if (!isLeftBtnDown)
+            isRightBtnDown = true;
+
+    }
+
+    public void OnRightBtnUp()
+    {
+        isRightBtnDown = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector3.zero;
+
+        if (isLeftBtnDown || isRightBtnDown)
+            move.x = isLeftBtnDown ? -1 : 1;
+        
+
+        charController.SimpleMove(move * speed * Time.deltaTime);
+    }
+}
