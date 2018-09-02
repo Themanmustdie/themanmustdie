a107da65d04bcfa95a7acda9bdd2a14e5a360ec4
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-03 02:35:05 +0800

    蜡烛燃烧

---
+++ Assets/Scripts/alan/CandleBurning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleBurning : MonoBehaviour {
+
+    public GameObject candleLight;
+    private Light light_;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickCandle() {
+        print("click");
+        light_ = candleLight.GetComponent<Light>();
+        light_.intensity = 40;
+    }
+}
