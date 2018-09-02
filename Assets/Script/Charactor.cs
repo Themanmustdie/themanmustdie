30a1d6442529cc157fa33a903ea816c718d5c3a8
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-02 12:32:33 +0800

    重玩关卡

---
+++ Assets/Script/Charactor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactor : MonoBehaviour {
+
+    private CharacterController cc;
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 targetDir = new Vector3(h, -10f, 0);
+        cc.Move(targetDir * speed);
+    }
+}
--- Assets/Scripts/bindokun/BtnSettingController.cs
+++ Assets/Scripts/bindokun/BtnSettingController.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BtnSettingController : MonoBehaviour {
 
     public GameObject scrollView;
-
+ 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
