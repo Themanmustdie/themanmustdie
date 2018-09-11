6fd32f0baf6048dfcfa20c55425b0d0f5b544480
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-11 23:25:31 +0800

    关卡3-1 脚本

---
+++ Assets/Scripts/alan/AddDownForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddDownForce : MonoBehaviour {
+    public GameObject pre; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void AddForce(){
+        GameObject clone = Instantiate(pre);
+        Rigidbody rb = clone.GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0, -290, 0), ForceMode.Impulse);
+        Destroy(clone, 0.2f);
+    }
+}
---
+++ Assets/Scripts/alan/AddGravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGravity : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //进入触发器执行的代码
+        if(collider.tag == "ice cube"){
+            Rigidbody rb = collider.GetComponent<Rigidbody>();
+            rb.mass = 10f;
+        }
+    }
+
+}
