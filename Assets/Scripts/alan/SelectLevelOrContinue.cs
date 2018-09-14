fd2cac1cc496c83cc6b7eb4ccfd28d0ab51daf07
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-14 23:27:53 +0800

    把继续游戏和选择关卡加上

---
+++ Assets/Scripts/alan/SelectLevelOrContinue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectLevelOrContinue : MonoBehaviour {
+    public GameObject selectlevel;
+    public Button contin;
+    public Button select;
+
+    // Use this for initialization
+    void Start()
+    {
+        contin.onClick.AddListener(Continue);
+        select.onClick.AddListener(Select);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Continue()
+    {
+        NetCtrl.instance.LoadScene(User.ID, User.Scene);
+    }
+
+    public void Select()
+    {
+        gameObject.SetActive(false);
+        selectlevel.SetActive(true);
+    }
+
+}
