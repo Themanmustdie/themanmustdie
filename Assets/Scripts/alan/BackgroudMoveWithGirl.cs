e6b83c83b1ba202095268832deb835f451988d94
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-14 19:09:26 +0800

    3-2背景随人物缓慢移动

--- Assets/Scripts/alan/BackgroudMoveWithGirl.cs
+++ Assets/Scripts/alan/BackgroudMoveWithGirl.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class BackgroudMoveWithGirl : MonoBehaviour {
     public GameObject girl;
+    public float lastX;
 	// Use this for initialization
 	void Start () {
-		
+        lastX = girl.transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float x = girl.transform.position.x;
+        float distance = x - lastX;
+        if(Mathf.Abs(distance) > 0.05){
+            Vector3 position_ = new Vector3(transform.position.x + distance * 2, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, position_, Mathf.Abs(distance));
+        }
+        lastX = x;
 	}
 }
