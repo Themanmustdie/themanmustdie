1141bf02ad88c3515997368a4b77958a7f2133d6
Themanmustdie <42464467+Themanmustdie@users.noreply.github.com>
2018-09-15 13:58:13 +0800

    第二关部分动画和优化

---
+++ Assets/Scripts/bindokun/ComicController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComicController : MonoBehaviour {
+
+    public GameObject[] comics = new GameObject[7];
+
+    private int curIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void OnClickComic()
+    {
+        if (curIndex >= comics.Length)
+        {
+            return;
+        }
+        if(curIndex > 0)
+        {
+           // comics[curIndex - 1].
+        }
+       
+    }
+
+}
