87d74d6e238b0eb4b63ec7b8732262336a8346ff
ouyanghanyan <ithinkily@vip.qq.com>
2018-09-16 19:03:47 +0800

    3-1 3-2场景自然衔接

--- Assets/Scripts/alan/BackgroudMoveWithGirl.cs
+++ Assets/Scripts/alan/BackgroudMoveWithGirl.cs
@@ -15,7 +15,7 @@ public class BackgroudMoveWithGirl : MonoBehaviour {
         float x = girl.transform.position.x;
         float distance = x - lastX;
         if(Mathf.Abs(distance) > 0.05){
-            Vector3 position_ = new Vector3(transform.position.x - distance * 2, transform.position.y, transform.position.z);
+            Vector3 position_ = new Vector3(transform.position.x - distance * 6, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, position_, Mathf.Abs(distance));
         }
         lastX = x;
